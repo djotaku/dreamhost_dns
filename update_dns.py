2599b9c5259c82de4bc5a02c85ef0514d0f2efd3
Eric Mesa <ericsbinaryworld@gmail.com>
2021-07-26 18:33:32 -0400

    changed logging output to use f-strings

--- update_dns.py
+++ update_dns.py
@@ -11,7 +11,6 @@ import json
 import re
 import requests
 import sys
-import syslog
 import uuid
 import logging
 
@@ -32,28 +31,28 @@ def get_dns_records(api_key):
 def del_dns_record(domain, dns_ip, api_key, protocol='ip'):
     current_ip = dns_ip
     rec_type = 'AAAA' if protocol == 'ipv6' else 'A'
-    logging.debug('The current %s IP is: %s', protocol, current_ip)
+    logging.debug(f'The current {protocol} IP for {domain} is: {current_ip}')
     if current_ip == '':
-        logging.error("Can't delete IP, value passed is empty")
+        logging.error(f"Can't delete IP for {domain}, value passed is empty")
         sys.exit("Weird")
     command = "dns-remove_record&record=" + domain + "&type=" + rec_type + "&value=" + current_ip
     response = speak_to_dreamhost(command, api_key)
     if response.get('result') == 'error':
         logging.error('Error while deleting %s record: \n %s', protocol, response)
-    logging.debug('Tried to del %s record and here is what Dreamhost responded: \n %s', protocol, response)
+    logging.debug(f'Tried to del {protocol} record for {domain} and Dreamhost responded: \n {response}')
 
 
 def add_dns_record(domain: str, new_ip_address: str, api_key: str, protocol='ip'):
     address = new_ip_address
     rec_type = "AAAA" if protocol == "ipv6" else "A"
-    logging.debug('Our new %s address is: %s', protocol, address)
+    logging.debug(f'Our new {protocol} address for {domain} is {address}')
     command = "dns-add_record&record=" + domain + "&type=" + rec_type + "&value=" + address
     response = speak_to_dreamhost(command, api_key)
     if response.get('result') == 'error':
-        logging.error('Error while adding %s record: \n %s', protocol, response)
+        logging.error(f'Error while adding {protocol} record for {domain}: \n {response=}')
     elif response.get('result') == 'success':
         logging.info("Record supposedly updated")
-    logging.debug('Tried to add %s record and Dreamhost responded with: \n %s', protocol, response)
+    logging.debug(f'Tried to add {protocol} record for {domain} and Dreamhost responded with: \n{response}')
 
 
 def update_dns_record(domain: str, dns_ip: str, new_ip_address: str, api_key, protocol='ip'):
@@ -70,7 +69,7 @@ def make_url_string(command, api_key):
 def speak_to_dreamhost(command, api_key):
     """Send command to dreamhost and get back the results as a JSON object."""
     api_url = "api.dreamhost.com"
-    logging.debug('Will try to speak to Dreamhost, here is what I will tell: %s', command)
+    logging.debug(f'I will send {command} to Dreamhost')
     substring = make_url_string(command, api_key)
     result = requests.get(f"https://{api_url}{substring}")
     logging.debug(f"Here is what Dreamhost responded: {result.json()}")
@@ -95,7 +94,7 @@ def clean_html(raw_html):
 def make_it_so(api_key: str, domains: str):
     if api_key == '' or domains is False:
         msg = 'API_Key and/or domain empty. Edit settings.json and try again.'
-        syslog.syslog(syslog.LOG_ERR, msg)
+        logging.error(msg)
         sys.exit(msg)
     current_dns_records = get_dns_records(api_key)
     domain_dns_ip_pairs = [(record.get("record"), record.get("value"))
@@ -112,13 +111,13 @@ def make_it_so(api_key: str, domains: str):
             logging.info(f"{domain} should go from {dns_ip} to {new_ip_address}")
             update_dns_record(domain, dns_ip, new_ip_address, api_key)
         else:
-            logging.info('IP Record up-to-date.')
+            logging.info(f'IP Record for {domain} is up-to-date.')
         if CHECK_IP_V6 == 1:
             new_ip_address = get_host_ip_address('ipv6')
             if dns_ip != new_ip_address:
                 update_dns_record(domain, dns_ip, new_ip_address, api_key, 'ipv6')
             else:
-                logging.info('IPv6 Record up-to-date.')
+                logging.info(f'IPv6 Record for {domain} is up-to-date.')
 
 
 def gather_information():
